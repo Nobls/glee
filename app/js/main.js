761ca41a6e0a42cc63e240132c1d5ebdc7a37807
Nobls <sergikon92@gmail.com>
2021-05-31 02:16:01 +0300

    added filter-category filter-price

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,35 @@
-$('.slider-top__inner').slick({
-  arrows: false,
-  dots: true,
-  fade: true,
-  autoplay: true,
-  autoplaySpeed: 3000
+$(function () {
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $('.slider-top__inner').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+  
+  var mixer1 = mixitup('.products-gallery__items', {
+  
+  });
+  
+  var mixer2 = mixitup('.new-design__items', {
+  
+  });
+
+  
 });
 
-var mixer1 = mixitup('.products-gallery__items', {
 
-});
-
-var mixer2 = mixitup('.new-design__items', {
-
-});
--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,8 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
